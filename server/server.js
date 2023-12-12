0bd5d9ca39ec12557a3c8d846f757d179cc21017
Emmssz <150143962+Emmssz@users.noreply.github.com>
2023-12-12 16:23:43 +0100

    Test making a server port 3000

--- server/server.js
+++ server/server.js
@@ -6,3 +6,30 @@
     "editor.minimap.maxColumn"; 80,
     "editor.defaultFormatter"; "esbenp.prettier-vscode"
   }
+
+  const express = require('express'); 
+  const server = express(); 
+  const PORT = 3000; 
+  
+ 
+  server
+    .use(express.json()) 
+    .use(express.urlencoded({ extended: false })) 
+    .use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', '*');
+      res.header('Access-Control-Allow-Methods', '*');
+      next();
+    });
+  
+ 
+  server.get('/users', (req, res) => {
+   
+  });
+  
+
+  server.listen(PORT, () => {
+    console.log(`Jamming to https://localhost:${PORT}`);
+    
+  });
+  
