e2af91bdcb1c4ad77144e06326bf03f89f97788b
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-12-14 14:17:07 +0100

    database

--- server/server.js
+++ server/server.js
@@ -40,27 +40,25 @@ server.get('/',(req, res)=>{
 });
 
 server.get('/users', (req,res) => {
-
+  // Upggift 3 - Steg 1
+  //deklarerar variabeln db och kopplar den till filen med databasen
+  const db = new sqlite3.Database('./gik339-labb2.db');
+  const sql = 'SELECT * FROM users';
+  //Uppgift 3 - steg 2, 3 och 4
+  // Funktion för att kunna fråga SQL + felmeddelande
+  db.all(sql, (err, rows)=> {
+    if(err) {
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
+  });
 });
 
 
 // Uppgift 3
 //importerar npm paketet sqlite3 via variabeln sqlite3
 const sqlite3 = require ('sqlite3').verbose()
-
-// Upggift 3 - Steg 1
-//deklarerar variabeln db och kopplar den till filen med databasen
-const db = new sqlite3.Database('./gik339-labb2.db');
-const sql = 'SELECT * FROM users';
-//Uppgift 3 - steg 2, 3 och 4
-// Funktion för att kunna fråga SQL + felmeddelande
-db.all(sql, (err, rows)=> {
-  if(err) {
-    res.status(500).send(err);
-  } else {
-    res.send(rows);
-  }
-});
 
 //Uppgift 4 Testa backend
 
