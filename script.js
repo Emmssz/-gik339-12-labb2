ef23d8ec9a3b059e0ec250c658445cc25449b2b8
sara-lund <150947910+sara-lund@users.noreply.github.com>
2023-12-19 11:37:39 +0100

    upg 6 & 7, start av css

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-//Uppgift 6
-// deklarerar variabeln request och sparar ner länken till servern
-const request = new Request('http://localhost:3000/users');
-
-//funktion för att hämta data
-async function fetchData(){
-    const response = await fetch(request);
-}
+//Uppgift 6 och 7
+document.addEventListener('DOMContentLoaded', () => {
+    //Uppgift 7 - Steg 1
+    // skapar "divbox" i html
+    const userListUl = document.createElement('divBox');
+    
+    //Uppgift 6 - Steg 1 och 2
+    // använder fetch för att hämta data från servern
+    fetch("http://localhost:3000/users")
+    //Uppgift 6 - Steg 3 och 4
+      .then(response => response.json())
+      .then(users => {
+        //Uppgift 7 - Steg 2
+        // upprepar koden (till li) för varje element i listan, via map()
+        users.map(user => {
+            //Uppgift 7 - Steg 3
+            // lägger till info för li-elementen
+            let element = `<ul class="list" style="border: 3px ${user.color} solid">
+                <li class="list__info id">ID: ${user.id}</li>
+                <li class="list__info name">Name: ${user.firstName} ${user.lastName}</li>
+                <li class="list__info username">Username: ${user.username}</li>
+                <li class="list__info color">Color: ${user.color}</li>
+                </ul>`;
+            //Uppgift 7 - Steg 4
+            //lägger till det i html
+            divBox.insertAdjacentHTML('beforeend', element);
+        });
+      })
+      //fångar upp fel och skriver ut det i konsolen
+      .catch(error => console.error("Error fetching data:", error));
+  });
